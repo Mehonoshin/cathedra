b05561ebba8e9ffe58737971ee6ec26683495115
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-02-02 21:02:07 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cathedra::Application.config.secret_token = '9dfd7a165bafb054df9fdd16d56b423264cc6d376e26af4887d7ef352fd1f148ab975f25d555f1fdfaa4bb060aa69b36f58ea320812b9b4a1866e0b29a0e633b'
