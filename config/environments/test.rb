4cac94625ef9a19331f9feabc0607e37bc0893ad
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-02-20 23:39:14 +0400

    Precompile BS style

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,5 @@ Cathedra::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  config.assets.precompile += %w( cerulean.min.css )
 end
