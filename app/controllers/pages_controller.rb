793e2fd29c2185655362ade2b80757941cbaf3e6
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-03-06 23:48:24 +0400

    fix abilities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,18 @@
 # coding: utf-8
 class PagesController < ApplicationController
   load_and_authorize_resource
+  skip_authorize_resource :only => [:history, :staff]
 
   def show
     @page = Page.find_by_permalink(params[:id])
   end
 
   def history
-    authorize! :read, Page
     @page = Page.find_by_permalink("history")
     render :show
   end
 
   def staff
-    authorize! :read, Page
     @page = Page.find_by_permalink("staff")
     render :show
   end
