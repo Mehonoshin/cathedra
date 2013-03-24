10cf412c69bfc9f835e965b9e0b01f5bba075b35
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-03-24 22:51:17 +0400

    Удаление студентов

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,16 +4,24 @@ class StudentsController < ApplicationController
     @students = []
     if params[:department].present?
       @department = Department.find(params[:department])
-      @students = Student.where(department_id: params[:department], course: params[:course], group_num: params[:group])
+      @students = Student.where(department_id: params[:department], course: params[:course], group_num: params[:group]).order("name ASC")
     end
   end
 
   def create
     @student = Student.new(params[:student])
     if @student.save
-      redirect_to students_path(department: params[:student][:department_id], course: params[:student][:course], group: params[:student][:group_num]), notice: "Студент добавлен"
+      redirect_to students_path(department: params[:student][:department_id],
+                                course: params[:student][:course],
+                                group: params[:student][:group_num]), notice: "Студент добавлен"
     else
       raise "Ой, какая-то беда"
     end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.delete
+    redirect_to students_path(department: @student.department_id, course: @student.course, group: @student.group_num), notice: "Студент удален"
   end
 end
