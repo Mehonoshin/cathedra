cbfcd5785727661cce6fa36bcb1c1cba339bc8b5
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-05-09 12:28:53 +0400

    Added faculty to materials

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,6 @@
 class Material < ActiveRecord::Base
   CATEGORIES = ["lection_topics", "practice_topics", "exam_topics", "stud_society", "recommended"]
-  FACULTIES = ["a", "b", "c"]
+  FACULTIES = ["a", "b", "c", "d"]
   attr_accessible :category, :department, :file, :title, :priority
 
   mount_uploader :file, AttachmentUploader
